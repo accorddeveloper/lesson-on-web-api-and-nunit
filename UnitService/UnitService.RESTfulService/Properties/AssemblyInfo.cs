6bf3e938606dcd25bb1bc545c59914b3f446341f
Rafał Bielec <info@rafalbielec.com>
2016-03-15 22:16:00 +0000

    Revert "A commit to trigger build on TeamCity"
    This reverts commit ee1a0fe62e973b79d09307956264cc162369d091.

--- UnitService/UnitService.RESTfulService/Properties/AssemblyInfo.cs
+++ UnitService/UnitService.RESTfulService/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UnitService.RESTfulService")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("en-GB")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible to COM components. If
 // you need to access a type in this assembly from COM, set the ComVisible attribute to true on that type.
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 // 
 // You can specify all the values or you can default the Revision and Build Numbers by using the '*'
 // as shown below:
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
